e4ad31a864b8f934e84a9962aa746722077edc5e
SOURAV GHOSH <sg22945@gmail.com>
2022-07-03 23:46:18 +0530

    pushing for deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 //configuration of mongoose database
+require('dotenv').config()
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/habit-tracker");
+mongoose.connect(process.env.DATABASE||"mongodb://localhost:27017/habit-tracker");
 const db=mongoose.connection;
 db.on("err",()=>{console.log("Cant Connect to Database")})
 db.once("open",()=>{console.log("Successfuly Connected to Database")});
